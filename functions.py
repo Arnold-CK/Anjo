2d1fd045397d92b6386826c1fc0df4ee7ae92336
Arnold Kigonya <arnold.kigonya@asigmacapital.com>
2023-08-16 08:37:47 +0300

    1. Added authentication 2. Redesigned the navigation of the system

---
+++ functions.py
@@ -0,0 +1,31 @@
+import streamlit_authenticator as stauth
+
+
+def switch_page(page_name: str):
+    from streamlit.runtime.scriptrunner import RerunData, RerunException
+    from streamlit.source_util import get_pages
+
+    def standardize_name(name: str) -> str:
+        return name.lower().replace("_", " ")
+
+    page_name = standardize_name(page_name)
+
+    pages = get_pages("Home.py")  # OR whatever your main page is called
+
+    for page_hash, config in pages.items():
+        if standardize_name(config["page_name"]) == page_name:
+            raise RerunException(
+                RerunData(
+                    page_script_hash=page_hash,
+                    page_name=page_name,
+                )
+            )
+
+    page_names = [standardize_name(config["page_name"]) for config in pages.values()]
+
+    raise ValueError(f"Could not find page {page_name}. Must be one of {page_names}")
+
+
+# def hasher():
+#     hashed_passwords = stauth.Hasher(["tony123", "andrew456", "MIS4l1fe"]).generate()
+#     return hashed_passwords
--- main.py
+++ main.py
@@ -1,3 +1,68 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from streamlit_option_menu import option_menu as option_menu
+from yaml.loader import SafeLoader
 
-st.write("Hello World")
+import functions as fx
+
+# --- PAGE CONFIG ---
+
+st.set_page_config(page_title="Anjo Farms", page_icon="🫑", layout="centered")
+
+# --- USER AUTHENTICATION ---
+
+with open("./config.yaml") as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config["credentials"],
+    config["cookie"]["name"],
+    config["cookie"]["key"],
+    config["cookie"]["expiry_days"],
+    config["preauthorized"],
+)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status:
+    with st.sidebar:
+        nav_bar = option_menu(
+            st.session_state["name"],
+            ["Dashboard", "Data Entry"],
+            icons=["bar-chart-line", "clipboard-data"],
+            menu_icon="person-circle",
+        )
+
+    if nav_bar == "Dashboard":
+        dashboard_option = option_menu(
+            menu_title=None,
+            options= ["Sales Analysis", "Costs Analysis"],
+            icons=["coin","card-text"],
+            orientation="horizontal"
+        
+    )
+        if dashboard_option == "Sales":
+            st.write("Sales dashboard")
+        if dashboard_option == "Costs":
+            st.write("Costs dashboard")
+
+    if nav_bar == "Data Entry":
+        entry_option = option_menu(
+            menu_title=None,
+            options= ["Sales Form", "Costs Form"],
+            icons=["journal-plus", "journal-minus"],
+            orientation="horizontal"
+        
+    )
+        if entry_option == "Sales":
+            st.write("Sales Form")
+        if entry_option == "Costs":
+            st.write("Costs Form")
+
+    authenticator.logout("Logout", "sidebar", key="unique_key")
+
+elif authentication_status is False:
+    st.error("Username/password is incorrect")
+elif authentication_status is None:
+    st.warning("Please enter your username and password")
