7782c17aa360ea4f52ba4eebd75807bbcd0f84a1
Arnold Kigonya <arnold.kigonya@asigmacapital.com>
2024-01-05 23:05:58 +0300

    1. Modularised the code 2. Implemented filtration functionality

--- functions.py
+++ functions.py
@@ -1,5 +1,14 @@
+import calendar
+from datetime import date
+from typing import List
+import datetime
+
+import pandas as pd
+import streamlit as st
 import streamlit_authenticator as stauth
-from datetime import datetime
+import yaml
+from gspread_dataframe import get_as_dataframe
+from yaml.loader import SafeLoader
 
 
 def switch_page(page_name: str):
@@ -29,20 +38,38 @@ def switch_page(page_name: str):
 
 def get_cost_categories():
     categories = [
-        "Seeds and Seedlings",
-        "Fertilisers and Nutrients",
-        "Labour and Salaries",
-        "Rent and Lease",
-        "Delivery to Customer",
-        "Maintenance and Repair",
+        "Seeds And Seedlings",
+        "Fertilisers And Nutrients",
+        "Labour And Salaries",
+        "Rent And Lease",
+        "Delivery",
+        "Maintenance And Repair",
         "Miscellaneous",
         "Utilities",
         "Giveaway Cost",
+        "Construction"
     ]
 
     categories.sort()
 
     return categories
+
+
+def get_years_since_2022():
+    current_year = date.today().year
+    year_list = list(range(2022, current_year + 1))
+    year_list.sort(reverse=True)
+    return year_list
+
+
+# @st.cache_data
+# def get_all_months():
+#     # return list(calendar.month_name)[1:]
+#     return list(range(1, 13))
+
+@st.cache_data
+def get_month_name_dict():
+    return {i: month_name for i, month_name in enumerate(calendar.month_name) if i != 0}
 
 
 def format_column(entry):
@@ -58,6 +85,120 @@ def format_date(input_date):
 
     return formatted_date
 
-# def hasher():
-#     hashed_passwords = stauth.Hasher(["tony123", "andrew456", "MIS4l1fe"]).generate()
-#     return hashed_passwords
+
+def set_page_config():
+    st.set_page_config(page_title="Anjo Farms", page_icon="🫑", layout="wide")
+
+
+def auth():
+    with open("./config.yaml") as file:
+        config = yaml.load(file, Loader=SafeLoader)
+
+    authenticator = stauth.Authenticate(
+        config["credentials"],
+        config["cookie"]["name"],
+        config["cookie"]["key"],
+        config["cookie"]["expiry_days"],
+        config["preauthorized"],
+    )
+
+    name, authentication_status, username = authenticator.login("Login", "main")
+
+    return name, authentication_status, username, authenticator
+
+
+def load_expense_data(expenses_sheet):
+    expenses_df = get_as_dataframe(expenses_sheet, parse_dates=True)
+
+    expenses_df = expenses_df.loc[:, ["data-bio_data-date", "data-bio_data-item",
+                                      "data-bio_data-cost_category",
+                                      "data-bio_data-total_cost", ]].dropna()
+
+    expenses_df["data-bio_data-cost_category"] = expenses_df["data-bio_data-cost_category"].apply(
+        format_column)
+
+    expenses_df['data-bio_data-date'] = pd.to_datetime(expenses_df['data-bio_data-date'], format='%d/%m/%y')
+
+    return expenses_df
+
+
+def process_category(expenses_df, category):
+    category_df = expenses_df[expenses_df["data-bio_data-cost_category"] == category]
+    category_df = category_df.sort_values(by="data-bio_data-date", ascending=False)
+    category_df["data-bio_data-date"] = category_df["data-bio_data-date"].apply(format_date)
+
+    category_df.rename(columns={
+        "data-bio_data-date": "Date",
+        "data-bio_data-item": "Item",
+        "data-bio_data-cost_category": "Cost Category",
+        "data-bio_data-total_cost": "Total Cost",
+    }, inplace=True)
+
+    category_df = category_df.reset_index(drop=True)
+    category_df.index += 1
+
+    return category_df
+
+
+def display_expander(category, category_df):
+    line_items = len(category_df)
+    total_cost = category_df["Total Cost"].sum()
+    formatted_total_cost = "{:,.0f}".format(total_cost)
+
+    with st.expander(f'{category} - {line_items} line items - {formatted_total_cost} ugx'):
+        st.dataframe(category_df, use_container_width=True)
+
+
+def filter_data(data: pd.DataFrame, filter_name: str, values: List[str]) -> pd.DataFrame:
+    if not values:
+        return data
+
+    if filter_name == "cost_categories":
+        data = data[data["data-bio_data-cost_category"].isin(values)]
+
+    if filter_name == "years":
+        data = data[data['data-bio_data-date'].dt.year.isin(values)]
+
+    if filter_name == "months":
+        data = data[data['data-bio_data-date'].dt.month.isin(values)]
+
+    if filter_name == "date_range":
+        data = data[data['data-bio_data-date']].isin(values)
+
+    return data
+
+
+def show_filters():
+    years = st.multiselect("Select Years", get_years_since_2022(),
+                           placeholder="You can choose multiple options", default=datetime.datetime.now().year)
+
+    months = st.multiselect("Select Months", get_month_name_dict().values(),
+                            placeholder="You can choose multiple options")
+
+    cost_categories = st.multiselect("Select Categories", get_cost_categories(),
+                                     placeholder="You can choose multiple options")
+
+    jan_1 = datetime.date(datetime.date.today().year, 1, 1)
+    dec_31 = datetime.date(datetime.date.today().year, 12, 31)
+
+    # date_range = st.date_input("Select a Date Range", value=(jan_1, dec_31),
+    #                            min_value=datetime.date(2022, 8, 1))
+
+    # start_date = st.date_input("Start Date (dd/mm/yyyy)", datetime.datetime.now(), format="DD/MM/YYYY",
+    #                            max_value=datetime.datetime.now(),
+    #                            min_value=datetime.date(2022, 8, 1))
+    #
+    # end_date = st.date_input("End Date (dd/mm/yyyy)", datetime.datetime.now(), format="DD/MM/YYYY",
+    #                          max_value=datetime.datetime.now(),
+    #                          min_value=datetime.date(2022, 8, 1))
+
+    return years, months, cost_categories
+
+
+def convert_date_range(date_tuple):
+    converted_dates = []
+    for date_str in date_tuple:
+        date_object = datetime.datetime.strptime(str(date_str), '%Y-%m-%d')
+        converted_date = date_object.strftime('%d/%m/%y')
+        converted_dates.append(converted_date)
+    return converted_dates
--- main.py
+++ main.py
@@ -1,43 +1,19 @@
-from datetime import datetime
-
 import altair as alt
 import gspread
 import pandas as pd
 import streamlit as st
-import streamlit_authenticator as stauth
-import yaml
-from gspread_dataframe import get_as_dataframe
 from millify import millify
-from pytz import timezone
 from streamlit_option_menu import option_menu as option_menu
-from yaml.loader import SafeLoader
 
 import functions as fx
 
-# --- PAGE CONFIG ---
+fx.set_page_config()
 
-st.set_page_config(page_title="Anjo Farms", page_icon="🫑", layout="wide")
-
-# --- USER AUTHENTICATION ---
-
-with open("./config.yaml") as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-authenticator = stauth.Authenticate(
-    config["credentials"],
-    config["cookie"]["name"],
-    config["cookie"]["key"],
-    config["cookie"]["expiry_days"],
-    config["preauthorized"],
-)
-
-name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, username, authenticator = fx.auth()
 
 if authentication_status:
 
-    # --- GET DATA & SETUP---
-
-    cost_categories = fx.get_cost_categories()
+    # cost_categories = fx.get_cost_categories()
     sheet_credentials = st.secrets["sheet_credentials"]
     gc = gspread.service_account_from_dict(sheet_credentials)
     current_user = st.session_state["name"]
@@ -47,10 +23,19 @@ if authentication_status:
     with st.sidebar:
         nav_bar = option_menu(
             current_user,
-            ["Costs", "Data Entry"],
-            icons=["bar-chart-line", "clipboard-data"],
+            ["Costs"],
+            icons=["bar-chart-line"],
             menu_icon="person-circle",
         )
+
+        with st.expander("Filters"):
+            years, months, cost_categories = fx.show_filters()
+
+            months = [k for k, v in fx.get_month_name_dict().items() if v in months]
+
+            # date_range = fx.convert_date_range(date_range)
+
+        st.divider()
 
     # --- DASHBOARDS ---
 
@@ -61,16 +46,14 @@ if authentication_status:
 
         expenses_sheet = anjo_workbook.worksheet("Expenses")
 
-        expenses_df = get_as_dataframe(expenses_sheet, parse_dates=True)
+        expenses_df = fx.load_expense_data(expenses_sheet)
 
-        expenses_df = expenses_df.loc[:, ["data-bio_data-date", "data-bio_data-item",
-                                          "data-bio_data-cost_category",
-                                          "data-bio_data-total_cost", ]].dropna()
-
-        expenses_df["data-bio_data-cost_category"] = expenses_df["data-bio_data-cost_category"].apply(
-            fx.format_column)
-
-        expenses_df['data-bio_data-date'] = pd.to_datetime(expenses_df['data-bio_data-date'], format='%d/%m/%y')
+        expenses_df = fx.filter_data(expenses_df, 'years', years)
+        expenses_df = fx.filter_data(expenses_df, 'months', months)
+        expenses_df = fx.filter_data(expenses_df, "cost_categories", cost_categories)
+        # expenses_df = fx.filter_data(expenses_df, "date_range", date_range)
+        # expenses_df = fx.filter_data(expenses_df, 'start_date', start_date)
+        # expenses_df = fx.filter_data(expenses_df, 'end_date', end_date)
 
         with details:
 
@@ -80,37 +63,10 @@ if authentication_status:
             df_cost_categories.sort()
 
             for category in df_cost_categories:
-                category_df = expenses_df[expenses_df["data-bio_data-cost_category"] == category]
-
-                category_df = category_df.sort_values(
-                    by="data-bio_data-date", ascending=False
-                )
-
-                category_df["data-bio_data-date"] = category_df["data-bio_data-date"].apply(fx.format_date)
-
-                category_df.rename(columns={
-                    "data-bio_data-date": "Date",
-                    "data-bio_data-item": "Item",
-                    "data-bio_data-cost_category": "Cost Category",
-                    "data-bio_data-total_cost": "Total Cost",
-                }, inplace=True)
-
-                category_df = category_df.reset_index(drop=True)
-                category_df.index += 1
-
-                line_items = len(category_df)
-                total_cost = category_df["Total Cost"].sum()
-                formatted_total_cost = "{:,.0f}".format(total_cost)
-
-                with st.expander(f'{category} - {line_items} line items - {formatted_total_cost} ugx'):
-                    st.dataframe(category_df, use_container_width=True)
+                category_df = fx.process_category(expenses_df, category)
+                fx.display_expander(category, category_df)
 
         with dashboard:
-
-            # monthly_costs_df = expenses_df.groupby(expenses_df['data-bio_data-date'].dt.strftime('%m'))[
-            #     'data-bio_data-total_cost'].sum().reset_index()
-            #
-            # monthly_costs_df.columns = ['Month', 'Total Cost']
 
             visuals_df = pd.DataFrame({
                 "Category": expenses_df["data-bio_data-cost_category"],
@@ -127,21 +83,18 @@ if authentication_status:
                     millify(total_costs, precision=2),
                 )
 
-                st.metric("Average monthly Cost", millify(total_costs, precision=2))
+                number_of_months = expenses_df['data-bio_data-date'].dt.month.nunique()
 
-                st.metric("Cost per Unit", millify(total_costs, precision=2))
+                average_monthly_cost = total_costs / number_of_months
 
-                st.metric("Cost of Goods Sold", millify(total_costs, precision=2))
+                st.metric("Average monthly Cost", millify(average_monthly_cost, precision=2))
 
-                # total_cost_per_category = expenses_df.groupby("data-bio_data-cost_category")[
-                #     "data-bio_data-total_cost"].sum()
-                # category_with_largest_total = total_cost_per_category.idxmax()
-                # st.metric("Average Monthly Cost", category_with_largest_total)
+                # st.metric("Cost per Unit", millify(total_costs, precision=2))
 
-
+                # st.metric("Cost of Goods Sold", millify(total_costs, precision=2))
 
             with pie_chart:
-                pie_chart = alt.Chart(visuals_df).mark_arc().encode(
+                pie_chart = alt.Chart(visuals_df).mark_arc(innerRadius=80).encode(
                     theta="sum(Cost (ugx))",
                     color="Category",
                     tooltip=[alt.Tooltip("Category", title="Category"),
@@ -181,112 +134,110 @@ if authentication_status:
 
     # --- DATA ENTRY FORMS ---
 
-    if nav_bar == "Data Entry":
-        entry_option = option_menu(
-            menu_title=None,
-            options=["Sales Form", "Costs Form"],
-            icons=["journal-plus", "journal-minus"],
-            orientation="horizontal",
-        )
-
-        # --- SALES FORM ---
-
-        if entry_option == "Sales Form":
-            st.write("Sales Form")
-
-        # --- COSTS FORM ---
-
-        if entry_option == "Costs Form":
-            item_key = "txtCostItem"
-            category_key = "slctCostCategory"
-            amount_key = "txtCostAmount"
-
-            with st.form(key="anjo_costs", clear_on_submit=True):
-                cost_date = st.date_input(
-                    label="Date", value=datetime.today(), format="DD-MM-YYYY"
-                )
-
-                st.write("---")
-
-                st.text_input(label="Item", disabled=False, key=item_key)
-
-                st.selectbox(
-                    label="Category", options=cost_categories, key=category_key
-                )
-
-                st.text_input(
-                    label="Amount",
-                    disabled=False,
-                    key=amount_key,
-                    placeholder="ugx",
-                )
-
-                submitted = st.form_submit_button("Save")
-
-                if submitted:
-                    is_valid = True
-                    cost_date = cost_date.strftime("%d-%b-%Y")
-
-                    with st.spinner("🔍 Validating form..."):
-                        item = st.session_state.get(item_key, "")
-                        category = st.session_state.get(category_key, "")
-                        amount = st.session_state.get(amount_key, "")
-
-                        if not item.strip():
-                            is_valid = False
-                            st.warning("⚠️ Item cannot be left blank")
-
-                        if not category.strip():
-                            is_valid = False
-                            st.warning("⚠️ Category cannot be left blank")
-
-                        if amount.strip():
-                            amount = float(amount.strip())
-                            if amount <= 0.0:
-                                is_valid = False
-                                st.warning("🚨 Please enter an Amount greater than zero")
-                        else:
-                            is_valid = False
-                            st.warning("⚠️ Amount cannot be left blank")
-
-                    if is_valid:
-                        st.info("👍 Form is Valid")
-
-                        with st.spinner("Saving Cost Data..."):
-                            timezone = timezone("Africa/Nairobi")
-
-                            timestamp = datetime.now(timezone).strftime(
-                                "%d-%b-%Y %H:%M:%S" + " EAT"
-                            )
-
-                            data = [
-                                cost_date,
-                                item,
-                                category,
-                                amount,
-                                current_user,
-                                timestamp,
-                            ]
-
-                            anjo_sheet = gc.open_by_key(st.secrets["sheet_key"])
-                            worksheet = anjo_sheet.worksheet("Costs")
-
-                            all_values = worksheet.get_all_values()
-
-                            next_row_index = len(all_values) + 1
-
-                            worksheet.append_row(
-                                data,
-                                value_input_option="user_entered",
-                                insert_data_option="insert_rows",
-                                table_range=f"a{next_row_index}",
-                            )
-
-                            st.success("✅ Cost data Saved Successfully!")
-
-                            # REDIRECT TO THE COSTS DASHBOARD
-
-    # --- LOGOUT ---
+    # if nav_bar == "Data Entry":
+    #     entry_option = option_menu(
+    #         menu_title=None,
+    #         options=["Sales Form", "Costs Form"],
+    #         icons=["journal-plus", "journal-minus"],
+    #         orientation="horizontal",
+    #     )
+    #
+    #     # --- SALES FORM ---
+    #
+    #     if entry_option == "Sales Form":
+    #         st.write("Sales Form")
+    #
+    #     # --- COSTS FORM ---
+    #
+    #     if entry_option == "Costs Form":
+    #         item_key = "txtCostItem"
+    #         category_key = "slctCostCategory"
+    #         amount_key = "txtCostAmount"
+    #
+    #         with st.form(key="anjo_costs", clear_on_submit=True):
+    #             cost_date = st.date_input(
+    #                 label="Date", value=datetime.today(), format="DD-MM-YYYY"
+    #             )
+    #
+    #             st.write("---")
+    #
+    #             st.text_input(label="Item", disabled=False, key=item_key)
+    #
+    #             st.selectbox(
+    #                 label="Category", options=cost_categories, key=category_key
+    #             )
+    #
+    #             st.text_input(
+    #                 label="Amount",
+    #                 disabled=False,
+    #                 key=amount_key,
+    #                 placeholder="ugx",
+    #             )
+    #
+    #             submitted = st.form_submit_button("Save")
+    #
+    #             if submitted:
+    #                 is_valid = True
+    #                 cost_date = cost_date.strftime("%d-%b-%Y")
+    #
+    #                 with st.spinner("🔍 Validating form..."):
+    #                     item = st.session_state.get(item_key, "")
+    #                     category = st.session_state.get(category_key, "")
+    #                     amount = st.session_state.get(amount_key, "")
+    #
+    #                     if not item.strip():
+    #                         is_valid = False
+    #                         st.warning("⚠️ Item cannot be left blank")
+    #
+    #                     if not category.strip():
+    #                         is_valid = False
+    #                         st.warning("⚠️ Category cannot be left blank")
+    #
+    #                     if amount.strip():
+    #                         amount = float(amount.strip())
+    #                         if amount <= 0.0:
+    #                             is_valid = False
+    #                             st.warning("🚨 Please enter an Amount greater than zero")
+    #                     else:
+    #                         is_valid = False
+    #                         st.warning("⚠️ Amount cannot be left blank")
+    #
+    #                 if is_valid:
+    #                     st.info("👍 Form is Valid")
+    #
+    #                     with st.spinner("Saving Cost Data..."):
+    #                         timezone = timezone("Africa/Nairobi")
+    #
+    #                         timestamp = datetime.now(timezone).strftime(
+    #                             "%d-%b-%Y %H:%M:%S" + " EAT"
+    #                         )
+    #
+    #                         data = [
+    #                             cost_date,
+    #                             item,
+    #                             category,
+    #                             amount,
+    #                             current_user,
+    #                             timestamp,
+    #                         ]
+    #
+    #                         anjo_sheet = gc.open_by_key(st.secrets["sheet_key"])
+    #                         worksheet = anjo_sheet.worksheet("Costs")
+    #
+    #                         all_values = worksheet.get_all_values()
+    #
+    #                         next_row_index = len(all_values) + 1
+    #
+    #                         worksheet.append_row(
+    #                             data,
+    #                             value_input_option="user_entered",
+    #                             insert_data_option="insert_rows",
+    #                             table_range=f"a{next_row_index}",
+    #                         )
+    #
+    #                         st.success("✅ Cost data Saved Successfully!")
+    #
+    #                         # REDIRECT TO THE COSTS DASHBOARD
 
     authenticator.logout("Logout", "sidebar", key="unique_key")
 
